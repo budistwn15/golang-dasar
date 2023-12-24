3cf1b43b2291bf6246211ab4b34cc2348bced6ad
budistwn15 <budi.setiawan112001@gmail.com>
2023-12-24 23:15:33 +0700

    type string go

---
+++ string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Budi")
+	fmt.Println("Budi Setiawan")
+
+	// menghitung karakter string
+	fmt.Println(len("Budi"))
+
+	// mengambil karakter pada posisi yang telah ditentukan
+	fmt.Println("Budi"[0])
+}
