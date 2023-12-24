5168a1e7f9138510a2ce29f26118d007d062a02d
budistwn15 <budi.setiawan112001@gmail.com>
2023-12-24 23:30:45 +0700

    constant go

---
+++ constanta.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const phi = 3.14
+	fmt.Println("Nilai phi adalah ", phi)
+
+	const (
+		price    = 500
+		discount = 2.14
+	)
+	fmt.Println(price)
+	fmt.Println(discount)
+}
