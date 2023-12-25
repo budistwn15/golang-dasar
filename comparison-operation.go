41d5a8e446b4b78bf261cc49473f20024d807646
budistwn15 <budi.setiawan112001@gmail.com>
2023-12-25 22:58:01 +0700

    comparison

---
+++ comparison-operation.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		x = 5
+		y = 10
+	)
+
+	fmt.Println(x > y)
+	fmt.Println(x < y)
+	fmt.Println(x >= y)
+	fmt.Println(x <= y)
+	fmt.Println(x == y)
+	fmt.Println(x != y)
+
+}
