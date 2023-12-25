8ea7c777d3b1b2ac4690ca6603c395cdae036341
budistwn15 <budi.setiawan112001@gmail.com>
2023-12-25 22:54:41 +0700

    math operation

---
+++ math-operation.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	var b = 5
+	var c = a + b
+	fmt.Println("Hasilnya adalah ", c)
+
+	var x = 5
+	x += 5
+	fmt.Println(x)
+
+	var y = -5
+	var z = +10
+
+	fmt.Println(y, z)
+
+	var isMerried = !false
+	fmt.Println(isMerried)
+
+}
